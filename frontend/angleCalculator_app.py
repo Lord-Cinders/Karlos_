0b0a24857dc8c1625347352d0afd6c681ff3a5c4
Lord-Cinders <vijayvardhan37@gmail.com>
2022-11-05 22:36:01 +0530

    files upload

---
+++ frontend/angleCalculator_app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+import paho.mqtt.publish as publish
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+def calculate_angle(a,b,c):
+        a = np.array(a) # First
+        b = np.array(b) # Mid
+        c = np.array(c) # End
+        
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians*180.0/np.pi)
+        
+        if angle >180.0:
+            angle = 360-angle
+            
+        return angle
+
+
+
+def main():
+
+    st.title("Pose detection app")
+    start_button=st.button("Start")
+    stop_button=st.button("stop")
+
+    
+    #calculating the angle
+
+    if start_button:
+        data_source_selection=st.selectbox("select your data source",options=('WebCam','video'))
+
+    # Network setup      
+        if data_source_selection == "WebCam":
+            cap= cv2.VideoCapture(0)
+            st.sidebar.text("WebCam")
+            st.sidebar.markdown("---")
+            codec=cv2.VideoWriter_fourcc('M','J','P','G')
+            out=cv2.video("output1.mp4",codec)
+            
+            
+            MQTT_SERVER = "192.168.0.102"
+            MQTT_PATH = "test_channel"
+                
+        
+            ## Setup mediapipe instance
+            with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+                while cap.isOpened():
+                    ret, frame = cap.read()
+                    
+                    # Recolor image to RGB
+                    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    image.flags.writeable = False
+                
+                    # Make detection
+                    results = pose.process(image)
+                
+                    # Recolor back to BGR
+                    image.flags.writeable = True
+                    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                    
+                    # Extract landmarks
+                    try:
+                        landmarks = results.pose_landmarks.landmark
+                        
+                        # Get coordinates
+                        hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                        shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                        elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                        wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                        
+                        # Calculate Shoulder angle
+                        Shoulder_angle = calculate_angle(hip, shoulder, elbow)
+                        print("Shoulder: ", Shoulder_angle)
+                        
+                        # Calculate elbow angle
+                        Elbow_angle = calculate_angle(shoulder, elbow, wrist)
+                        print("Elbow: ",Elbow_angle)
+                        
+                        # pushes data into raspberry pi
+                        #publish.single(MQTT_PATH, str(Elbow_angle) + " " + str(Shoulder_angle), hostname=MQTT_SERVER) 
+
+                        # Visualize angle
+                        cv2.putText(image, str(Elbow_angle), 
+                                    tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                        cv2.putText(image, str(Shoulder_angle), 
+                                    tuple(np.multiply(shoulder, [640, 480]).astype(int)), 
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                                
+                    except:
+                        pass
+                    
+                    
+                    # Render detections
+                    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) )               
+                    
+                    cv2.imshow('Mediapipe Feed', image)
+                    st.write(out)
+                    if stop_button:
+                        break
+                
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__=='__main__':
+    main()
