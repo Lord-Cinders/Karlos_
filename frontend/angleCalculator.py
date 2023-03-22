829416945b25ad54ed10b57a4f7b23a5a1839850
Lord-Cinders <vijayvardhan37@gmail.com>
2023-03-22 19:14:19 +0530

    add left hand coords

--- frontend/angleCalculator.py
+++ frontend/angleCalculator.py
@@ -51,36 +51,53 @@ with mp_pose.Pose(min_detection_confidence=0.85, min_tracking_confidence=0.85, m
             landmarks = results.pose_world_landmarks.landmark
             
             # Get coordinates
-            hip_xy_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,       landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            shoulder_xy_right  = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            elbow_xy_right    = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y ]
-            wrist_xy_right    = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y ]
+            hip_xy_right        = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,       landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            shoulder_xy_right   = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow_xy_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y ]
+            wrist_xy_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y ]
             
-            hip_yz_right     = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,       landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            shoulder_yz_right = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            elbow_yz_right    = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].z,     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-            wrist_yz_right    = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].z,     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y ]
+            hip_yz_right        = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            shoulder_yz_right   = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow_yz_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].z,     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            wrist_yz_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].z,     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y ]
 
-            hip_xy_left      = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-            shoulder_xy_left  = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow_xy_left     = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y ]
-            wrist_xy_left    = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y ]
+            nose_xyz = [landmarks[mp_pose.PoseLandmark.NOSE.value].x, landmarks[mp_pose.PoseLandmark.NOSE.value].y, landmarks[mp_pose.PoseLandmark.NOSE.value].z]
+
+            hip_xy_left         = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            shoulder_xy_left    = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow_xy_left       = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y ]
+            wrist_xy_left       = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y ]
             
-            hip_yz_left       = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].z,       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-            shoulder_yz_left  = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow_yz_left     = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].z,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            hip_yz_left         = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].z,       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            shoulder_yz_left    = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow_yz_left       = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].z,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            wrist_yz_left       = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].z,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y ]
 
 
-            # Calculate Shoulder angle
+            # Calculate right Shoulder angle
             Shoulder_angle_xy_right = calculate_angle(hip_xy_right, shoulder_xy_right, elbow_xy_right)
             Shoulder_angle_yz_right = calculate_angle(hip_yz_right, shoulder_yz_right, elbow_yz_right)
-            print("Shoulder: ", Shoulder_angle_xy_right, Shoulder_angle_yz_right)
-            
-            # Calculate elbow angle
+            print("Right Shoulder: ", Shoulder_angle_xy_right, Shoulder_angle_yz_right)
+            Right_Shoulder_angles = str(Shoulder_angle_xy_right) + ',' + str(Shoulder_angle_yz_right)     
+                       
+            # Calculate right elbow angle
             Elbow_angle_xy_right = calculate_angle(shoulder_xy_right, elbow_xy_right, wrist_xy_right)
             Elbow_angle_yz_right = calculate_angle(shoulder_yz_right, elbow_yz_right, wrist_yz_right)
-            #print("Elbow: ",Elbow_angle_xy_right, Elbow_angle_yz_right)
-            Elbow_angles = str(Elbow_angle_xy_right) + ',' + str(Elbow_angle_yz_right)
+            print("Right Elbow: ",Elbow_angle_xy_right, Elbow_angle_yz_right)
+            Right_Elbow_angles = str(Elbow_angle_xy_right) + ',' + str(Elbow_angle_yz_right)
+
+            # Calculate left Shoulder angle
+            Shoulder_angle_xy_left = calculate_angle(hip_xy_left, shoulder_xy_left, elbow_xy_left)
+            Shoulder_angle_yz_left = calculate_angle(hip_yz_left, shoulder_yz_left, elbow_yz_left)
+            print("Left Shoulder: ", Shoulder_angle_xy_left, Shoulder_angle_yz_left)
+            Left_Shoulder_angles = str(Shoulder_angle_xy_left) + ',' + str(Shoulder_angle_yz_left)
+            
+            # Calculate left elbow angle and nose
+            Elbow_angle_xy_left = calculate_angle(shoulder_xy_left, elbow_xy_left, wrist_xy_left)
+            Elbow_angle_yz_left = calculate_angle(shoulder_yz_left, elbow_yz_left, wrist_yz_left)
+            print("Left Elbow: ",Elbow_angle_xy_left, Elbow_angle_yz_left)
+            print("Nose: ", nose_xyz)
+            Left_Elbow_angles = str(Elbow_angle_xy_left) + ',' + str(Elbow_angle_yz_left)
 
             # pushes data into raspberry pi
             #publish.single(MQTT_PATH, str(Shoulder_angle_xy_right) + " " + str(Shoulder_angle_yz_right), hostname=MQTT_SERVER) 
@@ -92,6 +109,18 @@ with mp_pose.Pose(min_detection_confidence=0.85, min_tracking_confidence=0.85, m
                                 )
             cv2.putText(image, str(Shoulder_angle_yz_right), 
                            tuple(np.multiply(shoulder_yz_right, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            cv2.putText(image, str(Elbow_angle_xy_left), 
+                           tuple(np.multiply(elbow_xy_left, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            cv2.putText(image, str(Shoulder_angle_xy_left), 
+                           tuple(np.multiply(shoulder_xy_left, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            cv2.putText(image, str(nose_xyz), 
+                           tuple(np.multiply(nose_xyz, [640, 480]).astype(int)), 
                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                                 )
                        
