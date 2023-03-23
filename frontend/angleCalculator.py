f089c8a48a0cabcd8f854cba4e2a9865188ae261
Lord-Cinders <vijayvardhan37@gmail.com>
2023-03-24 03:26:30 +0530

    restructure code

--- frontend/angleCalculator.py
+++ frontend/angleCalculator.py
@@ -45,6 +45,13 @@ payload = ""
 ## Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.85, min_tracking_confidence=0.85, model_complexity=2) as pose:
     while cap.isOpened():
+        # Controller inputs
+        current_time = time.mktime(time.gmtime())
+
+        current_inputs = joy.read()
+        if current_inputs[-2] == 1 and current_time - passed_time >= 1:
+            joy.ControllerFlag *= -1
+
         ret, frame = cap.read()
         
         # Recolor image to RGB
@@ -58,43 +65,36 @@ with mp_pose.Pose(min_detection_confidence=0.85, min_tracking_confidence=0.85, m
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
-        # Controller inputs
-        current_time = time.mktime(time.gmtime())
-
-        current_inputs = joy.read()
-        if current_inputs[-2] == 1 and current_time - passed_time >= 1:
-            joy.ControllerFlag *= -1
-        
         #print(joy.ControllerFlag)
-
+        if(joy.ControllerFlag == -1):
         # Extract landmarks
-        try:
-            landmarks = results.pose_world_landmarks.landmark
-            
-            # Get coordinates
-            hip_xy_right        = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,       landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            shoulder_xy_right   = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            elbow_xy_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y ]
-            wrist_xy_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y ]
-            
-            hip_yz_right        = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            shoulder_yz_right   = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            elbow_yz_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].z,     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-            wrist_yz_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].z,     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y ]
+            try:
+                landmarks = results.pose_world_landmarks.landmark
+                
+                # Get coordinates
+                hip_xy_right        = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,       landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                shoulder_xy_right   = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbow_xy_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y ]
+                wrist_xy_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y ]
+                
+                hip_yz_right        = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                shoulder_yz_right   = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbow_yz_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].z,     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                wrist_yz_right      = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].z,     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y ]
 
-            nose_xyz = [landmarks[mp_pose.PoseLandmark.NOSE.value].x, landmarks[mp_pose.PoseLandmark.NOSE.value].z, landmarks[mp_pose.PoseLandmark.NOSE.value].y]
+                nose_xyz = [landmarks[mp_pose.PoseLandmark.NOSE.value].x, landmarks[mp_pose.PoseLandmark.NOSE.value].z, landmarks[mp_pose.PoseLandmark.NOSE.value].y]
 
-            hip_xy_left         = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-            shoulder_xy_left    = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow_xy_left       = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y ]
-            wrist_xy_left       = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y ]
-            
-            hip_yz_left         = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].z,       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-            shoulder_yz_left    = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow_yz_left       = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].z,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-            wrist_yz_left       = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].z,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y ]
+                hip_xy_left         = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                shoulder_xy_left    = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbow_xy_left       = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y ]
+                wrist_xy_left       = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y ]
+                
+                hip_yz_left         = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].z,       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                shoulder_yz_left    = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].z,  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbow_yz_left       = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].z,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                wrist_yz_left       = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].z,     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y ]
 
-            if(joy.ControllerFlag == -1):
+                
                 # Calculate right Shoulder angle
                 Shoulder_angle_xy_right = str(calculate_angle(hip_xy_right, shoulder_xy_right, elbow_xy_right))
                 Shoulder_angle_yz_right = str(calculate_angle(hip_yz_right, shoulder_yz_right, elbow_yz_right))
@@ -120,9 +120,36 @@ with mp_pose.Pose(min_detection_confidence=0.85, min_tracking_confidence=0.85, m
                 Left_Elbow_angles = str(Elbow_angle_xy_left) + ',' + str(Elbow_angle_yz_left)
 
                 payload = Right_Shoulder_angles + ',' + Elbow_angle_yz_right + ',' +Left_Shoulder_angles + ',' + Elbow_angle_yz_left
-            
 
-            else:
+                # Visualize angle
+                cv2.putText(image, str(Elbow_angle_yz_right), 
+                            tuple(np.multiply(elbow_yz_right, [640, 480]).astype(int)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                cv2.putText(image, str(Shoulder_angle_yz_right), 
+                            tuple(np.multiply(shoulder_yz_right, [640, 480]).astype(int)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                cv2.putText(image, str(Elbow_angle_xy_left), 
+                            tuple(np.multiply(elbow_xy_left, [640, 480]).astype(int)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                cv2.putText(image, str(Shoulder_angle_xy_left), 
+                            tuple(np.multiply(shoulder_xy_left, [640, 480]).astype(int)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                cv2.putText(image, str(nose_xyz), 
+                            tuple(np.multiply(nose_xyz, [640, 480]).astype(int)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                # Render detections
+                
+            
+                
+            except:
+                pass
+        
+        else:
                 #print(current_inputs)
                 shoulder_left_dxy   = str(current_inputs[0]     // 12)                       # Normalize to -11 to 10
                 shoulder__left_dzy  = str(current_inputs[1]     // 12)                       # Normalize to -11 to 10
@@ -133,51 +160,23 @@ with mp_pose.Pose(min_detection_confidence=0.85, min_tracking_confidence=0.85, m
                 
                 payload = shoulder_right_dxy + ',' + shoulder_right_dzy + ',' + elbow_right + ',' + shoulder_left_dxy + ',' + shoulder__left_dzy + ',' + elbow_left
             
-            print(payload)
-            # pushes data into raspberry pi
+        print(payload)
+        cv2.imshow('Mediapipe Feed', image)
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                        mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                        mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                        )               
+        
+        if cv2.waitKey(10) & 0xFF == ord('q') or current_inputs[-1]:
+            break
+
+        # pushes data into raspberry pi
             if (joy.ControllerFlag != 1 and NETWORKFLAG):
                 publish.single(MQTT_PATH, payload, hostname=MQTT_SERVER) 
             
             elif(joy.ControllerFlag == 1 and NETWORKFLAG):
                 publish.single(MQTT_PATH, payload, hostname=MQTT_SERVER)
 
-            # Visualize angle
-            cv2.putText(image, str(Elbow_angle_yz_right), 
-                           tuple(np.multiply(elbow_yz_right, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
-            cv2.putText(image, str(Shoulder_angle_yz_right), 
-                           tuple(np.multiply(shoulder_yz_right, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
-            cv2.putText(image, str(Elbow_angle_xy_left), 
-                           tuple(np.multiply(elbow_xy_left, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
-            cv2.putText(image, str(Shoulder_angle_xy_left), 
-                           tuple(np.multiply(shoulder_xy_left, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
-            cv2.putText(image, str(nose_xyz), 
-                           tuple(np.multiply(nose_xyz, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
-            
-                  
-        except:
-            pass
-        
-        
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                 )               
-        
-        cv2.imshow('Mediapipe Feed', image)
-        if cv2.waitKey(10) & 0xFF == ord('q') or current_inputs[-1]:
-            break
-        
         passed_time = current_time 
 
     cap.release()
