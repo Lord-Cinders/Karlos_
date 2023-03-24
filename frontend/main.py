040b65bf244257a6f265c745e2ba3eba4243d4c2
Sanyam Sharma <sanyamsh20@gmail.com>
2023-03-24 17:23:42 +0530

    Removed ascii art printing

--- frontend/main.py
+++ frontend/main.py
@@ -1,10 +1,5 @@
 import sys
 from karlos import karlos
 
-if __name__ == "__main__":
-
-    with open('karlosArt.txt') as art:
-        print(art.read())
-        art.close()
-    
+if __name__ == "__main__":    
     karlos(sys)
