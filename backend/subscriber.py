ab4d0f79074c7f37244609138180f759026bd3b2
Lord-Cinders <vijayvardhan37@gmail.com>
2023-03-25 00:26:37 +0530

    add network logic

--- backend/subscriber.py
+++ backend/subscriber.py
@@ -21,3 +21,27 @@ def init():
 
 def move_servos(angles):
     pass
+
+def on_connect(client, userdata, flags, rc):
+    print("CONNECTION ESTABLISHED "+str(rc))
+ 
+    # Subscribing in oneans that if_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_PATH)
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+    payload = msg.payload.split(',')
+    if payload[0] == 'pose':
+        pass
+    else:
+        pass
+
+
+init()
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(MQTT_SERVER)
+client.loop_forever()
